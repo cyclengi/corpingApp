2a8e0440a781743d2adbf6f36ba3308fca12eed1
cyclengi <tmikami2400@gmail.com>
2019-12-25 18:17:48 +0900

    Add ユーザ登録機能を追加
    ・ユーザ登録画面、サーブレットを作成し、ユーザ登録機能を追加した。
    ・2019/12/25　18:17現在、ユーザ登録に失敗するバグが発生している。
    
    ・コミット修正のテスト
    ・タグ付けのテスト

---
+++ WebContent/WEB-INF/jsp/logonResult.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ユーザ登録結果</title>
+</head>
+<body>
+
+<p>
+<c:out value="${requestScope.msg}" />
+</p>
+
+<p>
+<a href="/corpingApp/">ログイン画面へ</a>
+</p>
+
+</body>
+</html>
--- src/sevlet/SignUpServlet.java
+++ src/sevlet/SignUpServlet.java
@@ -2,11 +2,15 @@ package sevlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.UserBean;
+import model.UserListLogic;
 
 /**
  * Servlet implementation class signUpServlet
@@ -14,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/servlet/SignUpServlet")
 public class SignUpServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,16 +31,37 @@ public class SignUpServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/logon.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		String userId = request.getParameter("userId");
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+		String mail = request.getParameter("mail");
+
+		UserBean userBean = new UserBean(userId, pass, name, mail);
+
+		UserListLogic logic = new UserListLogic();
+		boolean result = logic.addUser(userBean);
+
+
+		if(result) {
+			request.setAttribute("msg", "ユーザ登録が完了しました。");
+		} else {
+			request.setAttribute("msg", "ユーザ登録に失敗しました。");
+		}
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/logonResult.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
 }
